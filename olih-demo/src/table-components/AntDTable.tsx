d3cb20500199d51f72ed04e4ba5967d2d24d9663
Kendy - CMC Global <thanhnhan.lionus@gmail.com>
2024-04-19 20:27:38 +0700

    update to antd basic table

--- olih-demo/src/table-components/AntDTable.tsx
+++ olih-demo/src/table-components/AntDTable.tsx
@@ -1,13 +1,7 @@
-import type {
-  ActionType,
-  EditableFormInstance,
-  ProColumns,
-  ProFormInstance,
-} from "@ant-design/pro-components";
-import { EditableProTable, ProCard, ProForm } from "@ant-design/pro-components";
+import type { ActionType, ProFormInstance } from "@ant-design/pro-components";
+import { ProCard, ProForm } from "@ant-design/pro-components";
 import React, { useRef, useState } from "react";
-import AntDPagination from "./AntDPagination";
-import { Button, Tag } from "antd";
+import { Button, Table, TableColumnsType, Tag } from "antd";
 import { defaultData } from "./fakeData/QuestionData";
 import { QuestionType } from "../user-types/QuestionType";
 
@@ -15,12 +9,10 @@ const AntDTable = () => {
   const [editableKeys, setEditableRowKeys] = useState<React.Key[]>(() => []);
   const formRef = useRef<ProFormInstance<any>>();
   const actionRef = useRef<ActionType>();
-  const editableFormRef = useRef<EditableFormInstance>();
-  const columns: ProColumns<QuestionType>[] = [
+  const columns: TableColumnsType<QuestionType> = [
     {
       title: "Friend Id",
       dataIndex: "friendlyId",
-      valueType: "text",
       ellipsis: true,
       fixed: true,
       width: "auto",
@@ -28,14 +20,12 @@ const AntDTable = () => {
     {
       title: "Question Bank",
       dataIndex: "associate",
-      valueType: "text",
       ellipsis: true,
       fixed: true,
     },
     {
       title: "Status",
       dataIndex: "status",
-      valueType: "text",
       ellipsis: true,
       render: (_, row) => {
         if (row?.status === "Active") {
@@ -60,80 +50,32 @@ const AntDTable = () => {
       title: "Category",
       key: "type",
       dataIndex: "type",
-      valueType: "select",
-      valueEnum: {
-        multiple: { text: "Default", status: "Default" },
-        radio: { text: "Warning", status: "Warning" },
-        vacant: {
-          text: "Error",
-          status: "Error",
-        },
-        judge: {
-          text: "Success",
-          status: "Success",
-        },
-      },
     },
     {
       title: "Question Number",
       dataIndex: "questionsNum",
-      valueType: "digit",
     },
     {
       title: "Scoring Method",
       dataIndex: "scoringMethod",
-      valueType: "select",
-      request: async () => [
-        {
-          value: "discrete",
-          label: "Discrete",
-        },
-        {
-          value: "continuous",
-          label: "Continue",
-        },
-      ],
-      fieldProps: (_, { rowIndex }) => {
-        return {
-          onSelect: () => {
-            editableFormRef.current?.setRowData?.(rowIndex, { fraction: [] });
-          },
-        };
-      },
     },
     {
       title: "Fraction",
       width: 150,
       dataIndex: "fraction",
-      valueType: (record) => {
-        const scoringMethod = record?.scoringMethod;
-        if (scoringMethod === "discrete") return "select";
-        return "digit";
-      },
-      fieldProps: {
-        mode: "multiple",
-      },
-      request: async () =>
-        ["A", "B", "D", "E", "F"].map((item, index) => ({
-          label: item,
-          value: index,
-        })),
     },
     {
       title: "Last Modified",
       dataIndex: "lastModified",
-      valueType: "date",
       ellipsis: true,
     },
     {
       title: "Created At",
       dataIndex: "created",
-      valueType: "date",
       ellipsis: true,
     },
     {
       title: "Option",
-      valueType: "option",
       fixed: "right",
       render: (_, row) => [
         <a
@@ -148,79 +90,43 @@ const AntDTable = () => {
             });
           }}
         >
-          <Button>Delete</Button>
+          Delete |
         </a>,
         <a
           key="edit"
           color="blue"
           onClick={() => {
-            debugger;
             actionRef.current?.startEditable(row.id);
           }}
         >
-          <Button>Edit</Button>
+          | Edit
         </a>,
       ],
     },
   ];
- const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
 
- const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
-   console.log("selectedRowKeys changed: ", newSelectedRowKeys);
-   setSelectedRowKeys(newSelectedRowKeys);
- };
- const rowSelection = {
-   selectedRowKeys,
-   onChange: onSelectChange,
- };
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
 
- return (
-   <ProCard>
-     <div
-       style={{
-         maxWidth: 1200,
-         margin: "auto",
-       }}
-     >
-       <ProForm<{
-         table: QuestionType[];
-       }>
-         formRef={formRef}
-         initialValues={{
-           table: defaultData,
-         }}
-       >
-         <EditableProTable<QuestionType>
-           rowKey="id"
-           scroll={{
-             x: 1400,
-             y: 900,
-           }}
-           rowSelection={rowSelection}
-           editableFormRef={editableFormRef}
-           controlled
-           actionRef={actionRef}
-           maxLength={10}
-           name="table"
-           columns={columns}
-           recordCreatorProps={{
-             record: (index) => {
-               return {
-                 id: index + 1,
-               };
-             },
-           }}
-           editable={{
-             type: "multiple",
-             editableKeys,
-             onChange: setEditableRowKeys,
-           }}
-         />
-         <AntDPagination></AntDPagination>
-       </ProForm>
-     </div>
-   </ProCard>
- );
+  return (
+    <Table<QuestionType>
+      rowKey="id"
+      scroll={{
+        x: 1400,
+        y: 900,
+      }}
+      rowSelection={rowSelection}
+      columns={columns}
+      dataSource={defaultData}
+    />
+  );
 };
 
 export default AntDTable;
